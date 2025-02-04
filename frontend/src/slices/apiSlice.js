472101a2ea610b4afae9cb93dc8ef0dfff854e3e
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-04 19:15:15 +0530

    d

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "https://ecommerce-app-mern-10.onrender.com" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "" }),
   tagTypes: ["Products","product","users"],
   endpoints: () => ({}),
 });

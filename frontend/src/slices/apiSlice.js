3a7e53115c09d27af380699ca9d15e6bbcc46f52
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-04 19:09:01 +0530

    d

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // Define a service using a base URL and expected endpoints
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "ecommerce-app-mern-aiyc.vercel.app" }),
   tagTypes: ["Products","product","users"],
   endpoints: () => ({}),
 });

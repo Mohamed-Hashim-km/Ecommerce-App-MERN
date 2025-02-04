8dfd551a75745381986a8e1114f9313165390e0c
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-04 19:12:50 +0530

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://ecommerce-app-mern-10.onrender.com",
+        target: "http://localhost:5000",
       },
     },
   },

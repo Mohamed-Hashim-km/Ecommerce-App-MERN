6568d13dcfac5fba9035919ebc47a3e6d8081e6d
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-04 19:55:02 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import dotenv from "dotenv";
 import connectDb from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
-import userRouts from "./routes/userRoutes.js";
-import { errorHandler } from "./middleWares/errorHandler.js";
-import cookieParser from "cookie-parser";
-import dotenv from "dotenv";
-import userOrder from "./routes/orderRoutes.js";
-import path from "path";
-import uploadRoutes from "./routes/uploadRoutes.js"
-import cors from "cors";
-
+import userRouts from "./routes/userRouts.js";
+import userOrder from "./routes/userOrder.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 dotenv.config();
 connectDb();
 const app = express();
-
 
 const allowedOrigins = ['https://ecommerce-app-mern-7ooz.vercel.app'];
 
@@ -26,13 +24,13 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true // Allow cookies to be sent with requests
 }));
 
 app.use(express.json());
 app.use(cookieParser());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("ecommerce");
@@ -43,12 +41,11 @@ app.use("/api/users", userRouts);
 app.use("/api/order", userOrder);
 app.use("/api/uploads", uploadRoutes);
 
-
-const __dirname=path.resolve()   // current folder path get publically  (ecommerce app kochi)
-app.use("/uploads",express.static(path.join(__dirname,"/uploads")))    //For publicilly accessable  
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.use(errorHandler);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });

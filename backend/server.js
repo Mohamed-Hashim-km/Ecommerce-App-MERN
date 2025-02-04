dd418201992e2d842aef7093118296d89579c7fb
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-04 20:27:05 +0530

    d

--- backend/server.js
+++ backend/server.js
@@ -22,15 +22,10 @@ app.use(cors({
 }));
 
 
-
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+
 
 const port = process.env.PORT;
 

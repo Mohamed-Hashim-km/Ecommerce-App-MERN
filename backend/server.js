7328ffbfadb4547a5d09ccc056b0b25970f6ab49
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-02-06 22:48:51 +0530

    first

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,15 @@ app.use(cors({
   credentials: true
 }));
 
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 

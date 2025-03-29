ebdfb4da61776006b20b1d74dd9710b7e856b1ed
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-03-29 10:28:34 +0530

    f

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,4 +3,16 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Increase chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor"; // Moves all node_modules dependencies into a separate chunk
+          }
+        },
+      },
+    },
+  },
 });

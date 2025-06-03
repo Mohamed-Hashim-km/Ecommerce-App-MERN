684e0f1f7eeacace48d1d48d8e1e5b1ec06f9131
Mohamed-Hashim-km <hashimhx3@gmail.com>
2025-06-03 11:11:45 +0530

    deploy

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,8 +9,8 @@ const generateToken = (res, userId) => {
 
     res.cookie("jwt", token, {
       httpOnly: true,
-       secure: true,
-      sameSite: "lax",
+      secure: true,
+      sameSite: "None",
       maxAge: 24 * 60 * 60 * 1000,
       // path: "/",
     });
